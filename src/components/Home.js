0481dc726cfc0339fbc572c16f11fbbb85336167
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-05-24 19:54:20 -0400

    Started modifying the app for sonal

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,31 +16,9 @@ export default function Home() {
   return (
     <div>
       <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
+        Hi my name is Sonal Ranjit. I am a software developer, recently focused on
+        build Augmented Reality applications using the Unity Game Engine. 
       </p>
-      <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
-      </p>
-      <div style={s.pageLinkContainer}>
-        <Interactive
-          as={Link}
-          {...s.link}
-          to="/example"
-        >Example page</Interactive>
-      </div>
-      <div style={s.pageLinkContainer}>
-        <Interactive
-          as={Link}
-          {...s.link}
-          to="/example/two-deep?field1=foo&field2=bar#boom!"
-        >Example two deep with query and hash</Interactive>
-      </div>
     </div>
   );
 }
